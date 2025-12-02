3634c293492a6fce9b181e06aaa2c2489ca74026
Carlosms <carloserico71@gmail.com>
2025-12-02 12:38:27 -0300

    add user registration endpoint and configure migrations

--- app/db_config.py
+++ app/db_config.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from sqlalchemy import String
